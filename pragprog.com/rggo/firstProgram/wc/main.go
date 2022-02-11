9aebabd17568f2b16d6ae9d40ff338547c91859e
bamboo-nova <suzuk1nov11@gmail.com>
2022-02-12 00:16:15 +0900

    exercise - count bytes & cross platform method

--- pragprog.com/rggo/firstProgram/wc/main.go
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -5,9 +5,15 @@ package main
 // `go test -v` and `go build`->wc executable in the directory
 // `echo "My first command line tool with Go" | ./wc`
 
-// Flowchart2
+// Flowchart2 (Adding Command-Line Flags)
 // add package `flag`
 // cat main.go | ./wc -l
+
+// Flowchart3 (compilling tools for different platforms)
+// GOOS=windows go build
+
+// Exercise
+// count bytes (cat main.go | ./wc -b)
 
 import (
 	"bufio"
@@ -20,15 +26,17 @@ import (
 func main() {
 	// Defining a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
+	// Defining a boolean flag -l to count lines instead of words
+	bytes := flag.Bool("b", false, "Count lines")
 	// Parsing the flags provided by the user
 	flag.Parse()
 
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
@@ -36,6 +44,11 @@ func count(r io.Reader, countLines bool) int {
 	// the scanner split type to words (default is split by lines)
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+
+	// Split by bytes
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Defining a counter
