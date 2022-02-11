e708077cda30566184448c24da0522d7b7657330
bamboo-nova <suzuk1nov11@gmail.com>
2022-02-12 00:00:37 +0900

    Adding Command-Line Flags

--- pragprog.com/rggo/firstProgram/wc/main.go
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -1,29 +1,42 @@
 package main
 
-// Flowchart
+// Flowchart1
 // make main.go and main_test.go
 // `go test -v` and `go build`->wc executable in the directory
 // `echo "My first command line tool with Go" | ./wc`
 
+// Flowchart2
+// add package `flag`
+// cat main.go | ./wc -l
+
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// Parsing the flags provided by the user
+	flag.Parse()
+
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count words so we define
+	// the scanner split type to words (default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Defining a counter
 	wc := 0
