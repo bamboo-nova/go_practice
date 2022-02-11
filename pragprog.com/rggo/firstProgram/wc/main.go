9bc7c23ac052a6314433ceca6adbb64565b023f1
bamboo-nova <suzuk1nov11@gmail.com>
2022-02-11 19:16:10 +0900

    first commit

---
+++ pragprog.com/rggo/firstProgram/wc/main.go
@@ -0,0 +1,38 @@
+package main
+
+// Flowchart
+// make main.go and main_test.go
+// `go test -v` and `go build`->wc executable in the directory
+// `echo "My first command line tool with Go" | ./wc`
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// Calling the count function to count the number of words
+	// received from the Standard Input and printing it out
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	// A scanner is used to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r)
+
+	// Define the scanner split type to words (default is split by lines)
+	scanner.Split(bufio.ScanWords)
+
+	// Defining a counter
+	wc := 0
+
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++
+	}
+
+	// Return the total
+	return wc
+}
